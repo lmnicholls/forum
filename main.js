45f67f9d37042ffbe66d739654b0fbd5785fe869
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-03-29 15:08:37 -0400

    bolded username, posts add to the top of div

--- main.js
+++ main.js
@@ -2,16 +2,27 @@
 var button = document.getElementsByTagName('button')[0];
 
 button.addEventListener('click', function () {
+  // access the textarea box
   var post = document.getElementsByTagName('textarea')[0].value;
+
+  // create a new p1 to hold the post
   var p1 = document.createElement('p');
+
+  // access the username box
   var userName = document.getElementsByTagName('input')[0].value;
+
+  // create a new p2 to hold the post
   var p2 = document.createElement('p');
+
+  // Add innter HTML to p1 and p2
   var text = "Posted by: ";
-  
   p1.innerHTML = post;
-  p2.innerHTML = text + userName;
-  console.log(p2);
+  p2.innerHTML = text + userName.bold();
+  
+  // access the div for posts
   const posts = document.getElementsByClassName("posts")[0];
-  posts.appendChild(p1);
-  posts.appendChild(p2);
+
+  // add posts to div element
+  posts.prepend(p2);
+  posts.prepend(p1);
 });
